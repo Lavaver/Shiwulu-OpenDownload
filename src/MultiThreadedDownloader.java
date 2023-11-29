dbe616e418f29e6d106568a49c50fbe329441b05
枫乔ccccc <108647963+Lavaver@users.noreply.github.com>
2023-11-29 11:51:00 +0800

    Add files via upload

--- src/MultiThreadedDownloader.java
+++ src/MultiThreadedDownloader.java
@@ -1,4 +1,13 @@
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.Toolkit;
+import java.awt.AWTException;
+import java.awt.PopupMenu;
+import java.awt.MenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.io.*;
+import java.nio.file.*;
 import java.net.URL;
 import java.net.HttpURLConnection;
 
@@ -7,15 +16,56 @@ import javax.swing.JOptionPane;
 public class MultiThreadedDownloader extends Thread {
     private String downloadUrl;
     private String savePath;
+    private TrayIcon trayIcon;
 
     public MultiThreadedDownloader(String downloadUrl, String savePath) {
         this.downloadUrl = downloadUrl;
         this.savePath = savePath;
     }
 
+    private boolean checkDiskSpace() {
+        File saveFolder = new File(savePath);
+        long requiredSpace = 0;
+
+        try {
+            URL url = new URL(downloadUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            requiredSpace = connection.getContentLength();
+
+            Path saveFolderPath = saveFolder.toPath();
+            FileStore store = Files.getFileStore(saveFolderPath);
+
+            long usableSpace = store.getUsableSpace();
+            if (requiredSpace > usableSpace) {
+                showDiskSpaceNotification(store, requiredSpace);
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    private void showDiskSpaceNotification(FileStore store, long requiredSpace) {
+        String drive = store.toString();
+        String title = drive + " 上的空间不足以下载此文件";
+        String content = "请考虑更换分区或硬盘并重试";
+
+        showBalloonNotification(title, content);
+    }
+
     @Override
     public void run() {
         try {
+            // 检查磁盘空间
+            if (!checkDiskSpace()) {
+                // 返回到输入存储路径窗口
+                main(new String[]{});
+                return;
+            }
+
             URL url = new URL(downloadUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
@@ -63,27 +113,26 @@ public class MultiThreadedDownloader extends Thread {
             input.close();
         } catch (Exception e) {
             String errorMessage = "下载时出现错误：" + e.getMessage();
-        String stackTrace = getStackTrace(e);
-        System.err.println(errorMessage);
-        System.err.println("堆栈日志输出的信息：");
-        System.err.println(stackTrace);
-        System.err.println("请不要将此控制台截图发给别人，这没有任何作用。");
-        System.err.println("你应该全选提交控制台输出的所有信息，并前往 https://github.com/Lavaver/Shiwulu-OpenDownload/issues 发 issues 以帮助作者快速定位问题并修复");
-        System.err.println("而且，对于部分诸如 CDN 镜像站、百度网盘、GitHub Releases 下载可能会直接报错 403 导致程序出错，请见谅！");
+            String stackTrace = getStackTrace(e);
+            System.err.println(errorMessage);
+            System.err.println("堆栈日志输出的信息：");
+            System.err.println(stackTrace);
+            System.err.println("请不要将此控制台截图发给别人，这没有任何作用。");
+            System.err.println("你应该全选提交控制台输出的所有信息，并前往 https://github.com/Lavaver/Shiwulu-OpenDownload/issues 发 issues 以帮助作者快速定位问题并修复");
+            System.err.println("而且，对于部分诸如 CDN 镜像站、百度网盘、GitHub Releases 下载可能会直接报错 403 导致程序出错，请见谅！");
 
-        JOptionPane.showMessageDialog(null, errorMessage, "下载出错", JOptionPane.ERROR_MESSAGE);
+            showErrorMessage(errorMessage);
         }
     }
 
     private void showNotification(String fileName, long fileSize, int progress, String message) {
         String title = fileName + " 的下载已" + (progress == 0 ? "开始" : "完成");
-        String content = (progress == 0 ? "文件大小：" + fileSize + "，" : "文件已下载到 " + savePath);
-        content += "总进度：" + progress + "%，剩余时间（ETA）：" + message;
-        JOptionPane.showMessageDialog(null, content, title, JOptionPane.INFORMATION_MESSAGE);
+        String content = (progress == 0 ? "文件大小：" + formatSize(fileSize) + "\n返回到控制台页面以查看详情。" : "文件已下载到 " + savePath + "\n你可能需要按下 Ctrl+C 来关闭此程序");
+        showBalloonNotification(title, content);
     }
 
     private void updateProgress(String fileName, long fileSize, long downloaded, int progress, String eta) {
-        String progressInfo = "已下载：" + downloaded + " / " + fileSize + "字节" + "，进度：" + progress + "%，剩余时间（ETA）：" + eta;
+        String progressInfo = "已接收：" + formatSize(downloaded) + " / " + formatSize(fileSize) + " 数据" + " | 进度：" + progress + "% | 剩余时间（ETA）：" + eta;
         System.out.println(progressInfo);
     }
 
@@ -94,6 +143,18 @@ public class MultiThreadedDownloader extends Thread {
         return remainingTime / 1000 + " 秒";
     }
 
+    private String formatSize(long size) {
+        if (size < 1024) {
+            return size + " 字节";
+        } else if (size < 1024 * 1024) {
+            return String.format("%.2f KB", size / 1024.0);
+        } else if (size < 1024 * 1024 * 1024) {
+            return String.format("%.2f MB", size / (1024.0 * 1024));
+        } else {
+            return String.format("%.2f GB", size / (1024.0 * 1024 * 1024));
+        }
+    }
+
     private String getStackTrace(Exception e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
@@ -101,20 +162,60 @@ public class MultiThreadedDownloader extends Thread {
         return sw.toString();
     }
 
-    public static void main(String[] args) {
-        String downloadUrl = JOptionPane.showInputDialog("请输入下载地址：\n请避免使用百度网盘或 GitHub 链接，因为会出错（");
-        if (downloadUrl == null) {
-            System.out.println("用户取消了下载操作，程序退出。");
-            return; // 用户取消输入，则退出程序
+    private void showBalloonNotification(String title, String message) {
+        if (!SystemTray.isSupported()) {
+            return;
         }
-    
+
+        SystemTray tray = SystemTray.getSystemTray();
+        try {
+            trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().getImage("icon.png"), "下载通知");
+            trayIcon.setImageAutoSize(true);
+
+            PopupMenu popupMenu = new PopupMenu();
+            MenuItem exitMenuItem = new MenuItem("退出");
+            exitMenuItem.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    tray.remove(trayIcon);
+                    System.exit(0);
+                }
+            });
+            popupMenu.add(exitMenuItem);
+            trayIcon.setPopupMenu(popupMenu);
+
+            tray.add(trayIcon);
+            trayIcon.displayMessage(title, message, TrayIcon.MessageType.INFO);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(null, message, "下载出错", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            String downloadUrl = JOptionPane.showInputDialog("请输入下载地址：\n请避免使用百度网盘或 GitHub 链接，因为会出错（");
+            if (downloadUrl == null) {
+return;
+}
+
         String savePath = JOptionPane.showInputDialog("请输入保存路径：");
         if (savePath == null) {
-            System.out.println("用户取消了输入保存路径，程序退出。");
-            return; // 用户取消输入，则退出程序
+            return;
         }
-    
+
         MultiThreadedDownloader downloader = new MultiThreadedDownloader(downloadUrl, savePath);
         downloader.start();
+    } else if (args.length == 2) {
+        String downloadUrl = args[0];
+        String savePath = args[1];
+
+        MultiThreadedDownloader downloader = new MultiThreadedDownloader(downloadUrl, savePath);
+        downloader.start();
+    } else {
+        System.out.println("请提供正确的参数：下载链接和保存路径");
     }
+}
 }
